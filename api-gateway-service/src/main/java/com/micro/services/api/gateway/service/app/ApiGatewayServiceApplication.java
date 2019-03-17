7ab846d74bf20efa2fbbcda7fb0c9b5de5ad578a
daviddai <david.dai.personal@gmail.com>
2019-03-17 12:46:05 +1100

    added missing annotations

--- api-gateway-service/src/main/java/com/micro/services/api/gateway/service/app/ApiGatewayServiceApplication.java
+++ api-gateway-service/src/main/java/com/micro/services/api/gateway/service/app/ApiGatewayServiceApplication.java
@@ -2,11 +2,15 @@ package com.micro.services.api.gateway.service.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableDiscoveryClient
+@EnableFeignClients(basePackages = {"com.micro.services.api.gateway.service.client"})
 @ComponentScan({"com.micro.services.api.gateway.service"})
 public class ApiGatewayServiceApplication {
 
